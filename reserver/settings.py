42b48ea23337e765ee5cad854a3de009b3996a85
bardia1373 <az.bardia13@gmail.com>
2014-10-27 20:04:21 +0330

    back to sqlite

--- reserver/settings.py
+++ reserver/settings.py
@@ -58,14 +58,11 @@ WSGI_APPLICATION = 'reserver.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Garson',
-        'USER': 'root',
-        'PASSWORD': '0212526',
-        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
