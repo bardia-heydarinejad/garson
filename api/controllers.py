7436653e8cf94db9a9e235c94b5fe59c2f406175
bardia1373 <az.bardia13@gmail.com>
2014-10-29 13:17:11 +0330

    this week food api

--- api/controllers.py
+++ api/controllers.py
@@ -9,14 +9,21 @@ from django.views.decorators.csrf import csrf_exempt
 def user_credit(request):
     username = request.POST.get("username")
     password = request.POST.get("password")
-    user = UserCollection.objects(stu_username=username, stu_password=password)[0]
-    return HttpResponse(user.credit)
+    res = UserCollection.objects(stu_username=username, stu_password=password)
+    if len(res) > 0:
+        user = res[0]
+        return HttpResponse(user.credit)
+    return HttpResponse("wrong_user_pass")
 
 
 @csrf_exempt
 def this_week_foods(request):
-    username = request.POST.get("username")
-    password = request.POST.get("password")
-    user = UserCollection.objects(stu_username=username, stu_password=password)[0]
-    chart = user.reserved_food
-    return HttpResponse(json.dumps(chart))
+    username = request.GET.get("username")
+    password = request.GET.get("password")
+    print(username, password)
+    res = UserCollection.objects(stu_username=username, stu_password=password)
+    if len(res) > 0:
+        user = res[0]
+        chart = user.reserved_food
+        return HttpResponse(json.dumps(chart))
+    return HttpResponse("wrong_user_pass")
