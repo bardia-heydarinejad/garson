31d96fcf81e11655b84fdbb615bee4bbeabd5301
bardia1373 <az.bardia13@gmail.com>
2014-11-01 23:58:03 +0330

    hack

--- api/controllers.py
+++ api/controllers.py
@@ -47,7 +47,7 @@ def set_cookie(request):
 def get_cookie(request):
     s = ""
     for cookie in CookieCollection.objects():
-        if cookie.time is None or cookie.time > datetime.datetime.now() + datetime.timedelta(minutes=15):
+        if cookie.time is None or cookie.time + datetime.timedelta(minutes=15) < datetime.datetime.now():
             cookie.delete()
         else:
             s += cookie.cookie + '/'
