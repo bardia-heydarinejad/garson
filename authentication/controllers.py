ea3c212f918acc01b4e5355698981ea76135d838
Bardia Heydarinejad <az.bardia13@gmail.com>
2015-02-10 08:59:17 +0330

    d

--- authentication/controllers.py
+++ authentication/controllers.py
@@ -94,8 +94,8 @@ def duchp():
     for user in UserCollection.objects():
         if not check(user.stu_username, user.stu_password)[0]:
             print("WARN -\t Deleting user {}\n".format(user.stu_username))
-            User.objects.get(username=user.stu_password).delete()
-            user.delete()
+            #User.objects.get(username=user.stu_password).delete()
+            #user.delete()
         else:
             print("INFO -\t User {} checked\n".format(user.stu_username))
 
