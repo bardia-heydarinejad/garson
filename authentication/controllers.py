ca00b6ba31fca01ce6d5b3ab199b9e2bf5fb063e
Bardia Heydarinejad <az.bardia13@gmail.com>
2015-02-16 09:23:08 +0330

    .

--- authentication/controllers.py
+++ authentication/controllers.py
@@ -90,7 +90,9 @@ def sync_mongo_sqlite():
     for user_in_mongo in UserCollection.objects():
         username = user_in_mongo.stu_username
         password = user_in_mongo.stu_password
-        if User.objects.get(username=username) is None:
+        try:
+            User.objects.get(username=username)
+        except:
             print('creating: {} in sqlite.'.format(username))
             user = User.objects.create_user(username=username, password=password)
             user.is_active = True
